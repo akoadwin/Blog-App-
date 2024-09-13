01517073492f6e09e414d25e959949b4d38a42cf
Godwin Mat-ao <adwinkoaclc@gmail.com>
2024-09-13 21:03:06 +0800

    Added Flutter Pick Image, 44000

--- lib/features/blog/presentation/pages/add_new_page.dart
+++ lib/features/blog/presentation/pages/add_new_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/pick_image.dart';
 import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +20,16 @@ class _AddNewBlogpageState extends State<AddNewBlogpage> {
   final titleController = TextEditingController();
   final contentController = TextEditingController();
   List<String> selectedTopics = [];
+  File? image;
+
+  Future<void> selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        image = pickedImage;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -41,34 +54,56 @@ class _AddNewBlogpageState extends State<AddNewBlogpage> {
           padding: const EdgeInsets.all(12.0),
           child: Column(
             children: [
-              DottedBorder(
-                strokeCap: StrokeCap.round,
-                borderType: BorderType.RRect,
-                radius: const Radius.circular(10),
-                color: AppPallete.borderColor,
-                dashPattern: const [10, 4],
-                child: Container(
-                  color: AppPallete.backgroundColor,
-                  height: 150,
-                  width: double.infinity,
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+              image != null
+                  ? GestureDetector(
+                      onTap: () {
+                        selectImage();
+                      },
+                      child: SizedBox(
+                        height: 250,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      SizedBox(
-                        height: 15,
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        selectImage();
+                      },
+                      child: DottedBorder(
+                        strokeCap: StrokeCap.round,
+                        borderType: BorderType.RRect,
+                        radius: const Radius.circular(10),
+                        color: AppPallete.borderColor,
+                        dashPattern: const [10, 4],
+                        child: Container(
+                          color: AppPallete.backgroundColor,
+                          height: 150,
+                          width: double.infinity,
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                "Select your image",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      Text(
-                        "Select your image",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
               const SizedBox(
                 height: 20,
               ),
@@ -93,6 +128,7 @@ class _AddNewBlogpageState extends State<AddNewBlogpage> {
                                 selectedTopics.add(item);
                                 setState(() {});
                               }
+                              print(selectedTopics);
                             },
                             child: Chip(
                               side: selectedTopics.contains(item)
